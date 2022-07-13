22c4f390592e211dedec385fdb44d2f0644f58a4
jamarob <janmartinrobert@gmail.com>
2022-07-13 13:34:35 +0200

    Setup routing

--- src/pages/LoginPage.js
+++ src/pages/LoginPage.js
@@ -1,11 +1,35 @@
+import {useState} from "react";
 import styled from "styled-components"
 
 export default function LoginPage({onLogin}){
+  const [credentials,setCredentials] = useState({
+    username: "",
+    password: ""
+  })
 
   return <Grid>
-    <button onClick={() => onLogin("toller-token")}>login</button>
+    <Form onSubmit={handleSubmit}>
+      <label>
+        Username
+        <input type="text" name="username" value={credentials.name} onChange={handleChange}/>
+      </label>
+      <label>
+        Password
+        <input type="password" name="password" value={credentials.password} onChange={handleChange}/>
+      </label>
+      <button>login</button>
+    </Form>
   </Grid>
 
+  function handleSubmit(event) {
+    event.preventDefault()
+    onLogin()
+  }
+
+  function handleChange(event) {
+    const {name,value} = event.target
+    setCredentials({...credentials, [name]: value})
+  }
 }
 
 const Grid = styled.div`
@@ -13,4 +37,13 @@ const Grid = styled.div`
   place-items: center;
   height: 100vh;
   padding: 12px;
+`
+
+const Form = styled.form`
+  display: grid;
+  gap: 12px;
+
+  input {
+    width: 100%;
+  }
 `
